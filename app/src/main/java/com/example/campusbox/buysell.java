372b10d5c0736743e534f37ebebdf07a231c5be7
unknown <rishabkshatri31@gmail.com>
2022-02-13 14:46:30 +0530

    test

---
+++ app/src/main/java/com/example/campusbox/buysell.java
@@ -0,0 +1,14 @@
+package com.example.campusbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class buysell extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buysell);
+    }
+}
